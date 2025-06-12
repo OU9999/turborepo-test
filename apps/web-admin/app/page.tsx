13dd60fc75a2fc39e559c9b65141a8c5f57474f4
오유진 <omh232323@wedatalab.com>
2025-06-12 10:07:11 +0900

    feat: init

--- apps/web-admin/app/page.tsx
+++ apps/web-admin/app/page.tsx
@@ -1,15 +1,11 @@
-import { TestQuery } from "@/components/test-query";
 import { Button } from "@workspace/ui/components/button";
-import { Input } from "@workspace/ui/components/input";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold bg-red-500">Hello World</h1>
-        <Button size="sm">Button</Button>
-        <Input />
-        <TestQuery />
+        <h1 className="text-2xl font-bold bg-blue-500">Hello! I AM ADMIN</h1>
+        <Button>TEST</Button>
       </div>
     </div>
   );
