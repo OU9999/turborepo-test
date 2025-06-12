f890ab8c33d32dbea1395e6477bff8fb30539813
오유진 <omh232323@wedatalab.com>
2025-06-12 16:54:21 +0900

    feat: 유틸 ts 및 export 적용

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,18 +1,20 @@
 import { Button } from '@workspace/ui/components/base/button';
+import { Input } from '@workspace/ui/components/base/input';
 import { CustomButton } from '@workspace/ui/components/button/custom-button';
+import { hello } from '@workspace/util';
 import Image from 'next/image';
 
 export default function Page() {
-  const hell = 'hel';
+  const message = hello();
+  console.log(message);
 
   return (
     <div className="flex min-h-svh items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-test bg-test2 text-4xl font-bold">
-          Hello World zz
-        </h1>
+        <h1 className="text-test bg-test2 text-4xl font-bold">Hello</h1>
         <Button size="sm">Button</Button>
         <CustomButton>HELLO!!</CustomButton>
+        <Input />
         <Image
           src="/test.png"
           alt="test"
