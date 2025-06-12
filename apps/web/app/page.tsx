0933ee40d15ec1f0f34a48542380cde5e7343d07
오유진 <omh232323@wedatalab.com>
2025-06-12 10:35:31 +0900

    test: 컬러 변수 테스트

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold bg-red-500">Hello World</h1>
+        <h1 className="text-2xl font-bold bg-test">Hello World</h1>
         <Button size="sm">Button</Button>
         <Input />
         <TestQuery />
