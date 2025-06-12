fe2d80f0c53e56da76bf210ddf812a97235f8484
오유진 <omh232323@wedatalab.com>
2025-06-12 11:33:03 +0900

    feat: standalone build 적용

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,21 @@
 import { Button } from "@workspace/ui/components/button";
 import { Input } from "@workspace/ui/components/input";
+import Image from "next/image";
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold bg-test">Hello World</h1>
+        <h1 className="text-2xl font-bold bg-test">Hello World zz</h1>
         <Button size="sm">Button</Button>
         <Input />
+        <Image
+          src="/test.png"
+          alt="test"
+          width={100}
+          height={100}
+          className="rounded-md"
+        />
       </div>
     </div>
   );
