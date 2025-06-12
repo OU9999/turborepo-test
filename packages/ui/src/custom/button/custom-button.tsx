4f36c1ed3e696f5baf0ec805ea736ca5cc11730b
오유진 <omh232323@wedatalab.com>
2025-06-12 16:03:46 +0900

    feat: 커스텀 ui export test

---
+++ packages/ui/src/custom/button/custom-button.tsx
@@ -0,0 +1,16 @@
+import { Button, ButtonProps } from '@workspace/ui/components/button';
+import { cn } from '@workspace/ui/lib/utils';
+
+interface CustomButtonProps extends ButtonProps {}
+
+const CustomButton = ({ className, ...props }: CustomButtonProps) => {
+  return (
+    <Button
+      aria-label="test"
+      className={cn('bg-test text-test2', className)}
+      {...props}
+    />
+  );
+};
+
+export { CustomButton };
