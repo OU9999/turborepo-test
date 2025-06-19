c33215185240c63aa48a3a1a5beb0d3e4db5eead
오유진 <omh232323@gmail.com>
2025-06-19 14:08:35 +0900

    fix: 유틸 import 변경

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,17 +2,13 @@ import { TestFooter } from '@/components/test-footer';
 import { Button } from '@workspace/ui/components/base/button';
 import { Input } from '@workspace/ui/components/base/input';
 import { CustomButton } from '@workspace/ui/components/button/custom-button';
-import { Util } from '@workspace/util';
-
 import Image from 'next/image';
 
 export default function Page() {
-  const date = Util.Date.formatDate('2025-05-13T13:47:59.039995115Z');
-
   return (
     <div className="flex min-h-svh items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-test bg-test2 text-4xl font-bold">{date}</h1>
+        <h1 className="text-test bg-test2 text-4xl font-bold">123</h1>
         <Button size="sm">Button</Button>
         <CustomButton>HELLO!!</CustomButton>
         <Input />
